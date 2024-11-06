1c9f638c757c6144e6a12496124a714cc6a786c0
ymarcon <yannick.marcon@epfl.ch>
2024-11-06 08:04:22 +0100

    chore: use enacit4r-pyutils

--- backend/api/models/campaigns.py
+++ backend/api/models/campaigns.py
@@ -1,60 +1,64 @@
 from typing import List, Optional, Tuple
 from pydantic import BaseModel
-from api.models.files import FileRef
+from enacit4r.models.files import FileRef
+
 
 class Reference(BaseModel):
-  citation: str
-  doi: str
-  
+    citation: str
+    doi: str
+
 
 class Measures(BaseModel):
-  name: str
-  description: Optional[str] = None
-  datasets: Optional[List[str]] = None
+    name: str
+    description: Optional[str] = None
+    datasets: Optional[List[str]] = None
 
 
 class Instrument(BaseModel):
-  name: str
-  description: Optional[str] = None
-  measures: Optional[List[Measures]]
+    name: str
+    description: Optional[str] = None
+    measures: Optional[List[Measures]]
+
 
 class TrackColumns(BaseModel):
-  latitude: str
-  longitude: str
-  timestamp: Optional[str] = None
+    latitude: str
+    longitude: str
+    timestamp: Optional[str] = None
+
 
 class Track(BaseModel):
-  file: FileRef
-  columns: TrackColumns
-  color: Optional[str] = None
-  timestamp_format: Optional[str]
+    file: FileRef
+    columns: TrackColumns
+    color: Optional[str] = None
+    timestamp_format: Optional[str]
 
 
 class Funding(BaseModel):
-  name: str
-  grant: Optional[str] = None
-  website: Optional[str] = None
+    name: str
+    grant: Optional[str] = None
+    website: Optional[str] = None
+
 
 class Campaign(BaseModel):
-  id: Optional[int] = None
-  name: str
-  acronym: str
-  website: Optional[str] = None
-  type: Optional[str] = None
-  color: Optional[str] = None
-  objectives: Optional[str] = None
-  start_date: Optional[str] = None
-  end_date: Optional[str] = None
-  location: Optional[str] = None
-  platform: Optional[str] = None
-  start_location: Tuple[float, float]
-  end_location: Optional[Tuple[float, float]] = None
-  images: Optional[List[FileRef]] = None
-  track: Optional[Track] = None
-  fundings: Optional[List[Funding]] = None
-  references: Optional[List[Reference]] = None
-  instruments: Optional[List[Instrument]] = None
+    id: Optional[int] = None
+    name: str
+    acronym: str
+    website: Optional[str] = None
+    type: Optional[str] = None
+    color: Optional[str] = None
+    objectives: Optional[str] = None
+    start_date: Optional[str] = None
+    end_date: Optional[str] = None
+    location: Optional[str] = None
+    platform: Optional[str] = None
+    start_location: Tuple[float, float]
+    end_location: Optional[Tuple[float, float]] = None
+    images: Optional[List[FileRef]] = None
+    track: Optional[Track] = None
+    fundings: Optional[List[Funding]] = None
+    references: Optional[List[Reference]] = None
+    instruments: Optional[List[Instrument]] = None
 
 
 class CampaignStore(BaseModel):
-  campaigns: List[str]
+    campaigns: List[str]
